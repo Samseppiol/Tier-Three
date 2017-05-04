c53415d75350ce8a77e819787b8adf51f6fea37e
Samseppiol <j_hage@live.com>
2017-05-04 11:31:54 +1000

    Changed views

--- db/migrate/20170504011655_add_index_to_business.rb
+++ db/migrate/20170504011655_add_index_to_business.rb
@@ -1,5 +1,5 @@
 class AddIndexToBusiness < ActiveRecord::Migration[5.0]
   def change
-    add_index(:businesses, :user_id, :unique => true)
+  #   add_index(:businesses, :user_id, :unique => true)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504005559) do
+ActiveRecord::Schema.define(version: 20170504011655) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "name"
